db75f75e864a5164f6d8d91a8fdd643584b90f12
sbukhovets <sbukhovets@gmail.com>
2020-11-10 17:35:06 +0300

    Added "arrRandomNumbers" method for creating array of numbers.

--- src/RandomNumbers.java
+++ src/RandomNumbers.java
@@ -2,14 +2,23 @@ import java.util.Scanner;
 
 public class RandomNumbers {
 
-    public static int randomNumbers(){
-        return 0;
+    public void arrRandomNumbers(int number){
+        int elemNumber = number;
+
+        int[] array = new int[elemNumber];
+        for (int i = 0; i < elemNumber; i++) {
+            array[i] = (int)(Math.random() * 20);
+            System.out.print(array[i] + " ");
+        }
     }
 
     public static void main(String[] args) {
-        Scanner numberCount = new Scanner(System.in);
+        Scanner inputNumber = new Scanner(System.in);
         System.out.println("Сколько случайных чисел будем генерировать: ");
-        int count = Integer.parseInt(numberCount.next());
-        System.out.println(count + " случайных чисел будет сгенерировано.");
+        int number = Integer.parseInt(inputNumber.next());
+        System.out.println(number + " случайных чисел будет сгенерировано.");
+
+        RandomNumbers randNum = new RandomNumbers();
+        randNum.arrRandomNumbers(number);
     }
 }
