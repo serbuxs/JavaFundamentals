d4f566d4ea1d9be3d76a34a33e2bf88cda6f2494
sbukhovets <sbukhovets@gmail.com>
2020-11-11 00:08:50 +0300

    Added "setArray" method with fill it by numbers.

--- src/RandomNumbers.java
+++ src/RandomNumbers.java
@@ -2,12 +2,28 @@ import java.util.Scanner;
 
 public class RandomNumbers {
 
-    public void arrRandomNumbers(int number){
-        int elemNumber = number;
+    public static int number;
+    int[] array = new int[number];
 
-        int[] array = new int[elemNumber];
-        for (int i = 0; i < elemNumber; i++) {
-            array[i] = (int)(Math.random() * 20);
+    public int[] setArray() {
+        for (int i = 0; i < number; i++) {
+            array[i] = (int) (Math.random() * 20);
+            System.out.println(array[i]);
+        }
+        return array;
+    }
+
+
+    // display array of numbers with transition on new line
+    public void displayArrayNewLine(int[] array) {
+        for (int i = 0; i < number; i++) {
+            System.out.println(array[i]);
+        }
+    }
+
+    // display array of numbers inline
+    public void displayArrayLine(int[] array) {
+        for (int i = 0; i < number; i++) {
             System.out.print(array[i] + " ");
         }
     }
@@ -15,10 +31,12 @@ public class RandomNumbers {
     public static void main(String[] args) {
         Scanner inputNumber = new Scanner(System.in);
         System.out.println("Сколько случайных чисел будем генерировать: ");
-        int number = Integer.parseInt(inputNumber.next());
-        System.out.println(number + " случайных чисел будет сгенерировано.");
+        number = Integer.parseInt(inputNumber.next());
+        System.out.println(number + " случайных чисел сгенерировано.");
 
         RandomNumbers randNum = new RandomNumbers();
-        randNum.arrRandomNumbers(number);
+        randNum.setArray();
+        //randNum.displayArrayNewLine();
+        //randNum.displayArrayLine();
     }
 }
