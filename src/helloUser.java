41c1e2aede58675abe0cf7f213fdbed2663fa7db
sbukhovets <sbukhovets@gmail.com>
2020-11-10 13:54:43 +0300

    Added "currentTime" and "userGreeting "methods.

--- src/helloUser.java
+++ src/helloUser.java
@@ -4,20 +4,18 @@ import java.util.Scanner;
 import java.time.LocalTime;
 
 public class helloUser {
-    public static void main(String[] args) {
-        //create object for user input
-        Scanner userInput = new Scanner(System.in);
 
-        System.out.print("Type your name please: ");
-        //get user input from console
-        String userName = userInput.next();
-
+    public int currentTime(){
         //get current time
         int currentTime = LocalTime.now().getHour();
         //System.out.println(currentTime);
+        return currentTime;
+    }
 
+    public void userGreeting(int currentTime, String userName){
+        int time = currentTime;
         // user greeting by current time
-        if (currentTime > 0 && currentTime < 5) {
+        if (time > 0 && currentTime < 5) {
             System.out.println("Good night, " + userName + "!");
         }
         else if (currentTime >= 5 && currentTime < 11) {
@@ -29,5 +27,17 @@ public class helloUser {
         else {
             System.out.println("Good evening, " + userName + "!");
         }
+    }
+
+    public static void main(String[] args) {
+        //create object for user input
+        Scanner userInput = new Scanner(System.in);
+
+        System.out.print("Type your name please: ");
+        //get user input from console
+        String userName = userInput.next();
+
+        helloUser user = new helloUser();
+        user.userGreeting(user.currentTime(), userName);
     }
 }
