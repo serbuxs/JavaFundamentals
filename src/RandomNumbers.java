928e629bfea4bae3bd2460c37fc112e7e95a5bcc
sbukhovets <sbukhovets@gmail.com>
2020-11-11 14:50:00 +0300

    Changed "setArray" method. Removed others methods. Added feature to display elements of array by two variants.

--- src/RandomNumbers.java
+++ src/RandomNumbers.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class RandomNumbers {
@@ -5,38 +6,26 @@ public class RandomNumbers {
     public static int number;
     int[] array = new int[number];
 
-    public int[] setArray() {
+    public void setArray() {
+        System.out.print("Элементы массива без перехода на новую строку: ");
         for (int i = 0; i < number; i++) {
             array[i] = (int) (Math.random() * 20);
-            System.out.println(array[i]);
-        }
-        return array;
-    }
-
-
-    // display array of numbers with transition on new line
-    public void displayArrayNewLine(int[] array) {
-        for (int i = 0; i < number; i++) {
-            System.out.println(array[i]);
-        }
-    }
-
-    // display array of numbers inline
-    public void displayArrayLine(int[] array) {
-        for (int i = 0; i < number; i++) {
             System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.print("Элементы массива с переходом на новую строку: \n");
+        for (int i = 0; i < number; i++) {
+            System.out.println(array[i]);
         }
     }
 
     public static void main(String[] args) {
         Scanner inputNumber = new Scanner(System.in);
-        System.out.println("Сколько случайных чисел будем генерировать: ");
+        System.out.println("Количество случайных чисел для генерации: ");
         number = Integer.parseInt(inputNumber.next());
-        System.out.println(number + " случайных чисел сгенерировано.");
+        System.out.println(number + " - случайных чисел сгенерировано.");
 
         RandomNumbers randNum = new RandomNumbers();
         randNum.setArray();
-        //randNum.displayArrayNewLine();
-        //randNum.displayArrayLine();
     }
 }
