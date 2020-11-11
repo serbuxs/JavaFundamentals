7f8955b88d967a83ea22958f260162961d63443c
sbukhovets <sbukhovets@gmail.com>
2020-11-11 16:25:24 +0300

    Added task description. Added empty "checkMonth" method.

--- src/MonthNumber.java
+++ src/MonthNumber.java
@@ -1,5 +1,18 @@
-public class MonthNumber {
-    public static void main(String[] args) {
+//Task: Ввести число от 1 до 12.
+// Вывести на консоль название месяца, соответствующего данному числу.
+// Осуществить проверку корректности ввода чисел.
 
+import java.util.Scanner;
+
+public class MonthNumber {
+
+    public void checkMonth() {
+
+    }
+
+    public static void main(String[] args) {
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.println(": ");
+        //number = Integer.parseInt(inputNumber.next());
     }
 }
