e0208bffe88c439716e9438a9a392b340488e654
sbukhovets <sbukhovets@gmail.com>
2020-11-13 14:31:45 +0300

    Added numbers input and sum of them.

--- src/MultiplySumNumbers.java
+++ src/MultiplySumNumbers.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class MultiplySumNumbers {
+    public static int number_a;
+    public static int number_b;
+    public static int number_c;
+    public static int number_d;
+
     public static void main(String[] args) {
-        
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("Введите числа: ");
+        number_a = inputNumber.nextInt();
+        number_b = inputNumber.nextInt();
+        number_c = inputNumber.nextInt();
+        number_d = inputNumber.nextInt();
+        System.out.print(number_a + number_b + number_c + number_d);
     }
 }
