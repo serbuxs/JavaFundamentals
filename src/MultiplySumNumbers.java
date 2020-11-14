e71b57403bf1e2c09bcfc9b38d1eeef2a2573499
sbukhovets <sbukhovets@gmail.com>
2020-11-15 01:12:58 +0300

    Added check for count of input elements. Added filling arrayList by elements and display it.

--- src/MultiplySumNumbers.java
+++ src/MultiplySumNumbers.java
@@ -1,24 +1,40 @@
 //Task: Ввести целые числа как аргументы командной строки,
 // подсчитать их сумму (произведение) и вывести результат на консоль.
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class MultiplySumNumbers {
 
-    public static void main(String[] args) {
-        Scanner inputNumber = new Scanner(System.in);
-        System.out.print("Введите числа: ");
+    public int argsCount() {
 
-        List<Integer> numbers = new ArrayList<Integer>();
-        for (int i = 0; i < 5; i++) {
-            int number = Integer.parseInt(inputNumber.next());
-            numbers.add(number);
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        int sum = 0;
+        int p = 1;
+        int i = 0;
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Scanner inputNumber = new Scanner(System.in);
+        System.out.print("Количество чисел: ");
+        int number = Integer.parseInt(inputNumber.next());
+        System.out.println("Указано количество - " + number + " чисел.");
+        if (number >= 2) {
+            System.out.print("Введите " + number + " чисел через пробел: ");
+            while (i < number){
+                numbers.add(inputNumber.nextInt());
+                i++;
+            }
+            System.out.print("Введенные числа: " + numbers.toString());
         }
-        System.out.print(numbers);
-        /*for (int i = 0; i < numbers.length; i++) {
-            System.out.print(numbers[i] + " ");
-        }*/
+        else if (number == 1) {
+            System.out.print("Недостаточное количество - " + number + " чисел ввода. Повторите ввод и введите минимум 2 числа.");
+        }
+        else{
+            System.out.print("Не введено ни одного числа. Повторите ввод и введите минимум 2 числа");
+        }
     }
 }
