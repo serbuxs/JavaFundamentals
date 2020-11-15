721609f15c0bb621ff8eaa8328e1000cb9e5bd28
sbukhovets <sbukhovets@gmail.com>
2020-11-15 21:41:47 +0300

    Code updated. Removed unused blocks.

--- src/MultiplySumNumbers.java
+++ src/MultiplySumNumbers.java
@@ -1,37 +1,25 @@
 //Task: Ввести целые числа как аргументы командной строки,
 // подсчитать их сумму (произведение) и вывести результат на консоль.
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 import java.util.ArrayList;
 
 public class MultiplySumNumbers {
 
-    ArrayList<Integer> numbers = new ArrayList<>();
-
-    /*public void sum(ArrayList numbers) {
-        int sum = 0;
-
-    }*/
-
-    public void checkInput(){
-
-    }
+    public static int number;
+    public static int sum = 0;
+    public static int multi = 1;
 
     public static void main(String[] args) {
-        int sum = 0;
-        int p = 1;
         int i = 0;
-        MultiplySumNumbers digit = new MultiplySumNumbers();
         ArrayList<Integer> numbers = new ArrayList<>();
         Scanner inputNumber = new Scanner(System.in);
         System.out.print("Количество чисел: ");
-        int number = Integer.parseInt(inputNumber.next());
+        number = Integer.parseInt(inputNumber.next());
         System.out.println("Указано количество - " + number + " чисел.");
         if (number >= 2) {
             System.out.print("Введите " + number + " чисел через пробел: ");
-            while (i < number){
+            while (i < number) {
                 numbers.add(inputNumber.nextInt());
                 i++;
             }
@@ -46,16 +34,12 @@ public class MultiplySumNumbers {
 
             //calculating numbers multiply
             for (int num : numbers) {
-                p *= num;
+                multi *= num;
             }
-            System.out.println("Произведение введенных чисел: " + p);
-
-            //digit.sum(numbers);
-        }
-        else if (number == 1) {
+            System.out.println("Произведение введенных чисел: " + multi);
+        } else if (number == 1) {
             System.out.print("Недостаточное количество - " + number + " чисел ввода. Повторите ввод и введите минимум 2 числа.");
-        }
-        else{
+        } else {
             System.out.print("Не введено ни одного числа. Повторите ввод и введите минимум 2 числа");
         }
         System.out.println("Работа программы завершена.");
