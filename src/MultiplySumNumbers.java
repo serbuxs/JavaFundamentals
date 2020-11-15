f36949d4d1cc3557632ad204cdd1e8904468aa14
sbukhovets <sbukhovets@gmail.com>
2020-11-15 22:52:05 +0300

    Added exception block.

--- src/MultiplySumNumbers.java
+++ src/MultiplySumNumbers.java
@@ -1,6 +1,7 @@
 //Task: Ввести целые числа как аргументы командной строки,
 // подсчитать их сумму (произведение) и вывести результат на консоль.
 
+import java.util.NoSuchElementException;
 import java.util.Scanner;
 import java.util.ArrayList;
 
@@ -18,26 +19,36 @@ public class MultiplySumNumbers {
         number = Integer.parseInt(inputNumber.next());
         System.out.println("Указано количество - " + number + " чисел.");
         if (number >= 2) {
-            System.out.print("Введите " + number + " чисел через пробел: ");
-            while (i < number) {
-                numbers.add(inputNumber.nextInt());
-                i++;
-            }
-            System.out.print("Введенные числа: " + numbers.toString());
-            System.out.println();
+            try {
+                System.out.print("Введите " + number + " целых чисел через пробел: ");
+                while (i < number) {
+                    numbers.add(inputNumber.nextInt());
+                    i++;
+                }
+                System.out.print("Введенные числа: " + numbers.toString());
+                System.out.println();
 
-            //calculating numbers sum
-            for (int num : numbers) {
-                sum += num;
-            }
-            System.out.println("Сумма введенных чисел: " + sum);
+                //calculating numbers sum
+                for (int num : numbers) {
+                    sum += num;
+                }
+                System.out.println("Сумма введенных чисел: " + sum);
 
-            //calculating numbers multiply
-            for (int num : numbers) {
-                multi *= num;
+                //calculating numbers multiply
+                for (int num : numbers) {
+                    multi *= num;
+                }
+                System.out.println("Произведение введенных чисел: " + multi);
             }
-            System.out.println("Произведение введенных чисел: " + multi);
-        } else if (number == 1) {
+            catch (NumberFormatException exp) {
+                System.err.print("Неверный формат введенного значения! Введите целые числа.");
+                inputNumber.nextLine();
+            } catch (NoSuchElementException exp) {
+                System.err.print("Не введено значение! Повторите попытку: ");
+                inputNumber.nextLine();
+            }
+        }
+        else if (number == 1) {
             System.out.print("Недостаточное количество - " + number + " чисел ввода. Повторите ввод и введите минимум 2 числа.");
         } else {
             System.out.print("Не введено ни одного числа. Повторите ввод и введите минимум 2 числа");
