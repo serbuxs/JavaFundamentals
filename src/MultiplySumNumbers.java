63c7dc11e739ee01836340b147fa857ded148eda
sbukhovets <sbukhovets@gmail.com>
2020-11-16 11:45:17 +0300

    Added input validation with exceptions. Rewritten error messages.

--- src/MultiplySumNumbers.java
+++ src/MultiplySumNumbers.java
@@ -16,43 +16,53 @@ public class MultiplySumNumbers {
         ArrayList<Integer> numbers = new ArrayList<>();
         Scanner inputNumber = new Scanner(System.in);
         System.out.print("Количество чисел: ");
-        number = Integer.parseInt(inputNumber.next());
-        System.out.println("Указано количество - " + number + " чисел.");
-        if (number >= 2) {
-            try {
-                System.out.print("Введите " + number + " целых чисел через пробел: ");
-                while (i < number) {
-                    numbers.add(inputNumber.nextInt());
-                    i++;
-                }
-                System.out.print("Введенные числа: " + numbers.toString());
-                System.out.println();
+        try {
+            number = Integer.parseInt(inputNumber.next());
+            System.out.println("Указано количество - " + number + " чисел.");
+            if (number >= 2) {
+                try {
+                    System.out.print("Введите " + number + " целых чисел через пробел: ");
+                    while (i < number) {
+                        numbers.add(inputNumber.nextInt());
+                        i++;
+                    }
+                    System.out.print("Введенные числа: " + numbers.toString());
+                    System.out.println();
 
-                //calculating numbers sum
-                for (int num : numbers) {
-                    sum += num;
-                }
-                System.out.println("Сумма введенных чисел: " + sum);
+                    //calculating numbers sum
+                    for (int num : numbers) {
+                        sum += num;
+                    }
+                    System.out.println("Сумма введенных чисел: " + sum);
 
-                //calculating numbers multiply
-                for (int num : numbers) {
-                    multi *= num;
+                    //calculating numbers multiply
+                    for (int num : numbers) {
+                        multi *= num;
+                    }
+                    System.out.println("Произведение введенных чисел: " + multi);
+                    System.out.println("Работа программы завершена успешно.");
                 }
-                System.out.println("Произведение введенных чисел: " + multi);
+                catch (NumberFormatException exp) {
+                    System.err.print("Ошибка ввода! Неверный формат введенного значения. Введите целые числа.");
+                    inputNumber.nextLine();
+                }
+                catch (NoSuchElementException exp) {
+                    System.err.print("Ошибка ввода! Допустимо вводить только целые числа.");
+                    inputNumber.nextLine();
+                }
             }
-            catch (NumberFormatException exp) {
-                System.err.print("Неверный формат введенного значения! Введите целые числа.");
-                inputNumber.nextLine();
-            } catch (NoSuchElementException exp) {
-                System.err.print("Не введено значение! Повторите попытку: ");
-                inputNumber.nextLine();
+            else if (number == 1) {
+                System.err.print("Недостаточное количество - " + number + " число ввода. Повторите ввод и введите минимум 2 числа.");
+            }
+            else if (number < 0) {
+                throw new ArithmeticException("Ошибка! Введено отрицательное значение количества чисел.");
+            }
+            else {
+                System.err.println("Не введено ни одного числа. Повторите ввод и введите минимум 2 числа");
             }
         }
-        else if (number == 1) {
-            System.out.print("Недостаточное количество - " + number + " чисел ввода. Повторите ввод и введите минимум 2 числа.");
-        } else {
-            System.out.print("Не введено ни одного числа. Повторите ввод и введите минимум 2 числа");
+        catch (NumberFormatException exp) {
+            System.err.println("Ошибка ввода! Неверное значение. Введите целое положительное число.");
         }
-        System.out.println("Работа программы завершена.");
     }
 }
