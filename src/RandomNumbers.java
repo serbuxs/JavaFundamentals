50dbd1cf655409c6f6d23d4c8cc3dbf49b5bf745
sbukhovets <sbukhovets@gmail.com>
2020-11-17 14:44:32 +0300

    Added comments.

--- src/RandomNumbers.java
+++ src/RandomNumbers.java
@@ -1,7 +1,6 @@
 //Task: Вывести заданное количество случайных чисел
 // с переходом и без перехода на новую строку
 
-import java.util.Arrays;
 import java.util.Scanner;
 
 public class RandomNumbers {
@@ -9,6 +8,7 @@ public class RandomNumbers {
     public static int number;
     int[] array = new int[number];
 
+    // output elements of Array
     public void setArray() {
         System.out.print("Элементы массива без перехода на новую строку: ");
         for (int i = 0; i < number; i++) {
@@ -25,10 +25,14 @@ public class RandomNumbers {
     public static void main(String[] args) {
         Scanner inputNumber = new Scanner(System.in);
         System.out.println("Количество случайных чисел для генерации: ");
+
+        // number input
         number = Integer.parseInt(inputNumber.next());
         System.out.println(number + " - случайных чисел сгенерировано.");
 
+        // create class object
         RandomNumbers randNum = new RandomNumbers();
+        // call method
         randNum.setArray();
     }
 }
