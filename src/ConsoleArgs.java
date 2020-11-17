bba8822a04b9d4dbaac1416ba37719a66c207638
sbukhovets <sbukhovets@gmail.com>
2020-11-17 13:59:18 +0300

    Added display for elements of ArrayList in reverse order.

--- src/ConsoleArgs.java
+++ src/ConsoleArgs.java
@@ -1,24 +1,29 @@
 //Task: Отобразить в окне консоли аргументы командной строки в обратном порядке.
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class ConsoleArgs {
 
     public void getArgs(int count) {
         int i = 0;
-        ArrayList<Integer> numbers = new ArrayList<>();
+        // create an ArrayList
+        ArrayList<String> numbers = new ArrayList<>();
+
         Scanner inputNumber = new Scanner(System.in);
         System.out.print("input: ");
+
+        // add elements to ArrayList
         while (i < count) {
-            numbers.add(inputNumber.nextInt());
+            numbers.add(inputNumber.next());
             i++;
         }
-        System.out.println(numbers.toString());
-        /*for ( ; i < count; i++) {
-            //System.out.print(numbers[i] + " ");
-            System.out.println(numbers.toString());
-        }*/
+        System.out.println(numbers);
+
+        // reverse the ArrayList
+        Collections.reverse(numbers);
+        System.out.println(numbers);
     }
 
     public static void main(String[] args) {
