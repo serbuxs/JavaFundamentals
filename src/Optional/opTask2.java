0bce99f339db8db0958898bd417c6bd2f65367f0
sbukhovets <sbukhovets@gmail.com>
2020-11-26 16:12:49 +0300

    Added package directive.

--- src/Optional/opTask2.java
+++ src/Optional/opTask2.java
@@ -2,6 +2,8 @@
 // Задать значения элементов матрицы в интервале значений от -M до M
 // с помощью генератора случайных чисел (класс Random)
 
+package Optional;
+
 public class opTask2 {
     public static void main(String[] args) {
 
