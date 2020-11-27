2dbb7d0813aa3350221d46a0ebddcd63ab496f89
sbukhovets <sbukhovets@gmail.com>
2020-11-28 02:30:54 +0300

    Added matrix filling by Random class generator.

--- src/Optional/opTask2.java
+++ src/Optional/opTask2.java
@@ -4,17 +4,19 @@
 
 package Optional;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class opTask2 {
 
     public static int n = 0; // array size value
-    public static int m = 15; // array range value
+    public static int m = 10; // array range value
+    public static final Random rand = new Random();
+
 
     public int[][] setArray(int n) {
-        int[][] twoDimArray = new int[n][n];
         //System.out.println(twoDimArray);
-        return twoDimArray;
+        return new int[n][n];
     }
 
     public int[][] getArray(int [][] twoDimArray) {
@@ -22,7 +24,7 @@ public class opTask2 {
         for (int i = 0; i < twoDimArray.length; i++) {
             for (int j = 0; j < twoDimArray[i].length; j++){
                 // fill matrix by random numbers
-                twoDimArray[i][j] = (int)(Math.random() * m) - m/2;
+                twoDimArray[i][j] = (int) (Math.random() * m - m/2 + 1);
                 System.out.print(" " + twoDimArray[i][j] + " ");
             }
             System.out.println();
