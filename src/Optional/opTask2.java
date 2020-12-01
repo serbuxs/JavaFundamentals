bccfdc4b02372b2942e13fc215c072d932d33e1f
sbukhovets <sbukhovets@gmail.com>
2020-12-01 17:43:49 +0300

    Changed updArray method.

--- src/Optional/opTask2.java
+++ src/Optional/opTask2.java
@@ -4,13 +4,13 @@
 
 package Optional;
 
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
+import java.lang.Object;
 
 public class opTask2 {
 
     public static int n = 0; // array size value
-    public static int m = 10; // array range value
+    public static int m = 20; // array range value
     public static final Random rand = new Random();
     public static int max = 0; // max number in array
 
@@ -21,27 +21,49 @@ public class opTask2 {
         return twoDimArray;
     }
 
-    public int[][] getArray(int [][] twoDimArray) {
+    public int[][] getArray(int[][] twoDimArray) {
         System.out.println("Элементы матрицы: ");
         for (int i = 0; i < twoDimArray.length; i++) {
-            for (int j = 0; j < twoDimArray[i].length; j++){
+            for (int j = 0; j < twoDimArray.length; j++) {
                 // fill matrix by random numbers
-                twoDimArray[i][j] = (int) (Math.random() * m - m/2 + 1);
+                twoDimArray[i][j] = (int) (Math.random() * m - m / 2 + 1);
                 System.out.print(" " + twoDimArray[i][j] + " ");
             }
             System.out.println();
         }
         return twoDimArray;
     }
-    
-    public int[][] updArray(int [][] twoDimArray) {
-        for (int i = 0; i < twoDimArray.length; i++) {
-            for (int j = 0; j < twoDimArray[i].length; j++) {
-                max = Math.max(max, twoDimArray[i][j]);
+
+    public void updArray(int[][] twoDimArray) {
+        //convert array to ArrayList
+        List<List<Integer>> lists = new ArrayList<>();
+        for (int[] ints : twoDimArray) {
+            List<Integer> list = new ArrayList<>();
+            for (int i : ints) {
+                list.add(i);
+
             }
+            lists.add(list);
         }
-        System.out.println("max element in array: " + max);
-        return twoDimArray;
+        //return Arrays.asList(twoDimArray).indexOf(max); //ArrayUtils.indexOf(twoDimArray, max);
+        //System.out.println("max element in array: " + max + " ");
+        // System.out.println();
+        System.out.println(lists);
+
+        for (int i = 0; i < twoDimArray.length; i++) {
+            int max = 0;
+            for (int j = 0; j < twoDimArray[i].length; j++) {
+
+                //max = Math.max(max, twoDimArray[i][j]);
+                if (twoDimArray[i][j] < twoDimArray[i+1][j+1]){
+                    max = twoDimArray[i+1][j+1];
+                }
+                else if (twoDimArray[i][j] > twoDimArray[i+1][j+1]) {
+                    break;
+                    }
+                }
+            System.out.print(max);
+        }
     }
 
     public static void main(String[] args) {
@@ -56,6 +78,6 @@ public class opTask2 {
         //input.getArray(input.setArray(n));
         input.updArray(input.getArray(input.setArray(n)));
 
-        System.out.println("Задать значение диапазона чисел: ");
+        // System.out.println("Задать значение диапазона чисел: ");
     }
 }
