4c1206a655a752e0162135a7a456210ad85be21d
sbukhovets <sbukhovets@gmail.com>
2020-12-02 16:54:30 +0300

    Updated getArray method for sort array elements. Removed unused code blocks.

--- src/Optional/opTask2.java
+++ src/Optional/opTask2.java
@@ -1,23 +1,24 @@
 // Task: Ввести с консоли n - размерность матрицы a [n] [n].
 // Задать значения элементов матрицы в интервале значений от -M до M
 // с помощью генератора случайных чисел (класс Random)
+// 1. Упорядочить строки (столбцы) матрицы
+// в порядке возрастания значений элементов k-го столбца (строки).
 
 package Optional;
 
 import java.util.*;
-import java.lang.Object;
 
 public class opTask2 {
 
     public static int n = 0; // array size value
-    public static int m = 20; // array range value
+    public static int m = 10; // array range value
+    public static int column = 1;
     public static final Random rand = new Random();
     public static int max = 0; // max number in array
 
 
     public int[][] setArray(int n) {
         int[][] twoDimArray = new int[n][n];
-        //System.out.println(twoDimArray);
         return twoDimArray;
     }
 
@@ -27,7 +28,7 @@ public class opTask2 {
             for (int j = 0; j < twoDimArray.length; j++) {
                 // fill matrix by random numbers
                 twoDimArray[i][j] = (int) (Math.random() * m - m / 2 + 1);
-                System.out.print(" " + twoDimArray[i][j] + " ");
+                System.out.print(twoDimArray[i][j] + " ");
             }
             System.out.println();
         }
@@ -35,34 +36,26 @@ public class opTask2 {
     }
 
     public void updArray(int[][] twoDimArray) {
-        //convert array to ArrayList
-        List<List<Integer>> lists = new ArrayList<>();
-        for (int[] ints : twoDimArray) {
-            List<Integer> list = new ArrayList<>();
-            for (int i : ints) {
-                list.add(i);
-
-            }
-            lists.add(list);
-        }
-        //return Arrays.asList(twoDimArray).indexOf(max); //ArrayUtils.indexOf(twoDimArray, max);
-        //System.out.println("max element in array: " + max + " ");
-        // System.out.println();
-        System.out.println(lists);
-
         for (int i = 0; i < twoDimArray.length; i++) {
             int max = 0;
-            for (int j = 0; j < twoDimArray[i].length; j++) {
-
-                //max = Math.max(max, twoDimArray[i][j]);
-                if (twoDimArray[i][j] < twoDimArray[i+1][j+1]){
-                    max = twoDimArray[i+1][j+1];
-                }
-                else if (twoDimArray[i][j] > twoDimArray[i+1][j+1]) {
-                    break;
+            for (int k = i + 1; k < twoDimArray.length; k++) {
+                if (twoDimArray[i][column] > twoDimArray[k][column]) {
+                    for (int j = 0; j < twoDimArray.length; j++) {
+                        int temp = twoDimArray[i][j];
+                        twoDimArray[i][j] = twoDimArray[k][j];
+                        twoDimArray[k][j] = temp;
                     }
                 }
-            System.out.print(max);
+            }
+        }
+
+        System.out.println();
+        System.out.println("Матрица после сортировки: ");
+        for (int i = 0; i < twoDimArray.length; i++ ){
+            for (int j = 0; j < twoDimArray.length; j++ ) {
+                System.out.print(twoDimArray[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
